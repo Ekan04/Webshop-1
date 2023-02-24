263d8f4cd9fcecddea71834c6647d9d033aa79da
Oscar Eklund <lordekan04@gmail.com>
2023-02-24 13:28:14 +0100

    added Employee, doesn t fully work tho

---
+++ Webshop/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Printing;
+using Webshop.Models;
+
+namespace Webshop.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string email)
+        {
+            Employee.GetEmployeeByEmail(email);
+            return View();
+        }
+    }
+}
---
+++ Webshop/Models/Employee.cs
@@ -0,0 +1,72 @@
+﻿using MySql.Data.MySqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Xml.Linq;
+
+namespace Webshop.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; } = -1;
+        public string Name { get; set; } = "";
+
+        [Required(ErrorMessage = "Writ a password")]
+        [Display(Name = "Your password")]
+        public string Password { get; set; } = "";
+
+        [Required(ErrorMessage = "Write a valid Emailadress")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "This Email's formatting is wrong")]
+        [Display(Name = "Your Emailadress")]
+        public string Email { get; set; } = "";
+        public string Role { get; set; } = "";
+
+
+
+        public static Employee GetEmployeeByEmail(string email)
+        {
+            string conStr = "Server = 46.246.45.183; User = OscarEk; Port = 3306; Database = OscarEk_DB; Password = SGRYGJBK";
+
+            using (MySqlConnection connection = new MySqlConnection(conStr))
+            {
+                connection.Open();
+
+                // Create a MySqlCommand object with the SELECT query to retrieve the employee data based on the email
+                string query = "SELECT * FROM Employee WHERE Email = @Email";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Email", email);
+
+                // Execute the SELECT query and get the results in a DataTable
+                DataTable dataTable = new DataTable();
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                {
+                    adapter.Fill(dataTable);
+                }
+
+                // Check if any rows were returned from the SELECT query
+                if (dataTable.Rows.Count > 0)
+                {
+                    // If yes, create a new Employee object and copy the data from the row into the object
+                    DataRow row = dataTable.Rows[0];
+                    Employee employee = new Employee()
+                    {
+                        Id = (int)row["Id"],
+                        Name = row["Name"].ToString(),
+                        Password = row["Password"].ToString(),
+                        Email = row["Email"].ToString(),
+                        Role = row["Role"].ToString()
+                    };
+                    return employee;
+
+                    // Use the employee object as needed
+                    // For example, you can access the employee's name using employee.Name
+                }
+                else
+                {
+                    ViewData["ErrorMessage"] = "Your Email or Password is wrong.";
+                    return View("LoginPartial", Employee);
+                }
+            }
+        }
+
+    }
+}
